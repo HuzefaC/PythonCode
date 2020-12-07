be941c9d0746e72577bec991cd906d392b8773d0
HuzefaC <huzefachabukswar@gmail.com>
2020-12-07 11:01:51 +0530

    Completed turtleOOP

--- turtleOOP/main.py
+++ turtleOOP/main.py
@@ -1,10 +1,19 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+#
+# kasu_the_turtle = Turtle()
+# kasu_the_turtle.shape("turtle")
+# kasu_the_turtle.color("purple")
+# kasu_the_turtle.forward(100)
+#
+# my_screen = Screen()
+# my_screen.exitonclick()
 
+from prettytable import PrettyTable
 
-kasu_the_turtle = Turtle()
-kasu_the_turtle.shape("turtle")
-kasu_the_turtle.color("purple")
-kasu_the_turtle.forward(100)
+table = PrettyTable()
 
-my_screen = Screen()
-my_screen.exitonclick()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
