5dd4819be16354c932a3e721ac22d189662b9045
HuzefaC <huzefachabukswar@gmail.com>
2020-12-11 18:21:39 +0530

    Completed pong_game

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -1,5 +1,10 @@
 from turtle import Turtle
 
+MOVEMENT_DISTANCE = 10
+MOVEMENT_SPEED = 0.1
+SPEED_INCREMENT_FACTOR = 0.9
+REVERSE_DIRECTIONS = -1
+
 
 class Ball(Turtle):
 
@@ -8,6 +13,22 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.move_x = MOVEMENT_DISTANCE
+        self.move_y = MOVEMENT_DISTANCE
+        self. move_speed = MOVEMENT_SPEED
 
     def move(self):
-        self.goto(self.xcor() + 10,  self.ycor() + 10)
+        self.goto(self.xcor() + self.move_x,  self.ycor() + self.move_y)
+
+    def bounce_y(self):
+        self.move_y *= REVERSE_DIRECTIONS
+
+    def bounce_x(self):
+        self.move_x *= REVERSE_DIRECTIONS
+        self.move_speed *= SPEED_INCREMENT_FACTOR
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.move_speed = MOVEMENT_SPEED
+
--- pong_game/main.py
+++ pong_game/main.py
@@ -1,10 +1,23 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 800
+
+BOUNCE_DISTANCE_TOP = 280
+BOUNCE_DISTANCE_BOTTOM = -280
+BOUNCE_DISTANCE_PADDLE = 50
+BOUNCE_DISTANCE_RIGHT = 320
+BOUNCE_DISTANCE_LEFT = -320
+
+RESET_DISTANCE_RIGHT = 360
+RESET_DISTANCE_LEFT = -360
+
+WIN_SCORE = 10
+
 
 PADDLE_LEFT_START_POSITION = (-350,  0)
 PADDLE_RIGHT_START_POSITION = (350,  0)
@@ -21,6 +34,7 @@ screen.setup(height=SCREEN_HEIGHT, width=SCREEN_WIDTH)
 paddle_left = Paddle(PADDLE_LEFT_START_POSITION)
 paddle_right = Paddle(PADDLE_RIGHT_START_POSITION)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(key="Up", fun=paddle_right.go_up)
@@ -31,8 +45,29 @@ screen.onkeypress(key="s", fun=paddle_left.go_down)
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    # Bounce off top or bottom
+    if ball.ycor() > BOUNCE_DISTANCE_TOP or ball.ycor() < BOUNCE_DISTANCE_BOTTOM:
+        ball.bounce_y()
+
+    # Bounce off the paddle
+    if (ball.distance(paddle_right) < BOUNCE_DISTANCE_PADDLE and ball.xcor() > BOUNCE_DISTANCE_RIGHT)\
+            or (ball.distance(paddle_left) < BOUNCE_DISTANCE_PADDLE and ball.xcor() < BOUNCE_DISTANCE_LEFT):
+        ball.bounce_x()
+    elif ball.xcor() > RESET_DISTANCE_RIGHT:
+        scoreboard.increase_score("left")
+        ball.reset_position()
+    elif ball.xcor() < RESET_DISTANCE_LEFT:
+        scoreboard.increase_score("right")
+        ball.reset_position()
+    elif scoreboard.left_score >= WIN_SCORE:
+        scoreboard.game_over("left")
+        game_is_on = False
+    elif scoreboard.right_score >= WIN_SCORE:
+        scoreboard.game_over("right")
+        game_is_on = False
 
 screen.exitonclick()
