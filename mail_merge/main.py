3518ab2b710ec6c47d9dae6e18fefcc1d959501c
HuzefaC <huzefachabukswar@gmail.com>
2020-12-23 17:59:39 +0530

    mail_merge completed

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -6,3 +6,15 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("Input/Names/invited_names.txt", mode="r") as file:
+    names = file.read().split(sep="\n")
+
+with open("Input/Letters/starting_letter.docx", mode="r") as file:
+    letter = file.read()
+
+for name in names:
+    with open(f"Output/ReadyToSend/{name}_letter.txt", mode="w") as file:
+        temp = letter.replace("[name]", name)
+        file.write(temp)
