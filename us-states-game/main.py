23388367717dcc84241be77271bb88597bb90653
HuzefaC <huzefachabukswar@gmail.com>
2020-12-25 16:38:08 +0530

    Completed us-states-game

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -9,13 +9,17 @@ turtle = Turtle(image)
 
 data = pandas.read_csv("50_states.csv")
 state_list = data.state.to_list()
-correct_count = 0
+correct_guess_list = []
+learn_list = []
 is_game_on = True
+
 while is_game_on:
-    guess = screen.textinput(title="Guess the state", prompt="Guess names of US state!!")
+    guess = screen.textinput(title=f"{len(correct_guess_list)}/50 state correct", prompt="Guess names of US state!!")
     guess = guess.title()
+    if guess == "Exit":
+        break
     if guess in state_list:
-        correct_count =
+        correct_guess_list.append(guess)
         new_turtle = Turtle()
         new_turtle.speed("slow")
         new_turtle.hideturtle()
@@ -23,5 +27,16 @@ while is_game_on:
         state_data = data[data.state == guess]
         new_turtle.goto(int(state_data.x), int(state_data.y))
         new_turtle.write(guess)
+    if len(correct_guess_list) == 50:
+        is_game_on = False
 
+for state in state_list:
+    if state not in correct_guess_list:
+        learn_list.append(state)
+
+learn_dict = {
+    "States": learn_list
+}
+df = pandas.DataFrame(learn_dict)
+df.to_csv("to_learn.csv")
 screen.mainloop()
