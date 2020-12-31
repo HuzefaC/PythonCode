3359623b59b176543032d70acb2fe8b4d5f30b1a
HuzefaC <huzefachabukswar@gmail.com>
2021-01-01 00:28:46 +0530

    Completed password_manageer

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
+import pyperclip
 # ---------------------------- CONSTANTS AND GLOBAL VARIABLES ------------------------------- #
 CANVAS_WIDTH = 200
 CANVAS_HEIGHT = 200
@@ -11,10 +12,55 @@ DARK = "#222831"
 YELLOW = "#ffd369"
 FONT_NAME = "Courier"
 
+password = []
+
+def validate():
+    website = website_entry.get()
+    email = email_entry.get()
+
+    if len(website) == 0 or len(email) == 0:
+        messagebox.showerror(title="Enter details!!", message="Please fill the details.")
+        return False
+    else:
+        return True
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
-    pass
+    # Password Generator Project
+    global password
+    password.clear()
+    if validate():
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u',
+                   'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+                   'P',
+                   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+        nr_letters = random.randint(4, 6)
+        nr_symbols = random.randint(1, 2)
+        nr_numbers = random.randint(1, 2)
+
+        for num in range(nr_letters):
+            random_char = random.choice(letters)
+            password += random_char
+
+        for num in range(nr_numbers):
+            random_number = random.choice(numbers)
+            password += random_number
+
+        for num in range(nr_symbols):
+            random_symbol = random.choice(symbols)
+            password += random_symbol
+
+        random.shuffle(password)
+        password_str = "".join(password)
+        password_entry.insert(0, password_str)
+        pyperclip.copy(password_str)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -23,7 +69,7 @@ def add_password():
     email = email_entry.get()
     password = password_entry.get()
 
-    if len(website) == 0 or len(password) == 0:
+    if validate() or len(password) == 0:
         messagebox.showerror(title="Enter details!!", message="Please fill the details.")
 
     else:
@@ -66,7 +112,7 @@ email_entry.insert(0, "huzefachabukswar@gmail.com")
 password_label = Label(text="Password:", bg=DARK, fg="white")
 password_label.grid(column=0, row=3)
 
-password_entry = Entry(width=24)
+password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3, sticky="W")
 
 password_button = Button(text="Generate Password", bg=DARK, fg="white", command=generate_password)
