61ccbf2b09e67f65f3273f9097beb03f22fcc240
HuzefaC <huzefachabukswar@gmail.com>
2021-01-13 10:38:27 +0530

    spofiry_playlist upadte

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.billboard.com/charts/hot-100/"
 
-response = requests.get(URL)
+date = input("What date do you want to go back to?:(Please use YYYY-MM-DD format for date) ")
+
+response = requests.get(URL + date)
 response.raise_for_status()
 
 website = response.text
@@ -12,7 +14,7 @@ soup = BeautifulSoup(website, "html.parser")
 song_list = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
 artist_list = soup.find_all(name="span", class_="chart-element__information__artist text--truncate color--secondary")
 
-print(song_list[0].getText())
+print(song_list[0].getText(), artist_list[0].getText())
 # for song in song_list:
 #     song_name = song.getText()
 #     artist =
