1b43c65eecbe4f53cb6d392139b795feb714999c
HuzefaC <huzefachabukswar@gmail.com>
2021-01-15 16:59:45 +0530

    Completed flask_intro

--- flask_intro/main.py
+++ flask_intro/main.py
@@ -1,6 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
+@app.route('/bye')
+def bye():
+    return 'Bye'
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+def make_bold(function):
+    pass
+
+
+@app.route('/<name>/<int:num>')
+def hello(name, num):
+    return f"Hello {name}. Number is {num}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
