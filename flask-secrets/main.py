f6932bb05cf8ddb018c7359fac7c7d4419935fe9
HuzefaC <huzefachabukswar@gmail.com>
2021-02-10 13:10:47 +0530

    Completed flask-secrets

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 
 class MyForm(FlaskForm):
@@ -12,6 +13,7 @@ class MyForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = "some secret string"
+Bootstrap(app)
 
 
 @app.route("/")
