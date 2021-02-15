74a1c2512e4f432b26aba8393c66f657b0e1e20e
HuzefaC <huzefachabukswar@gmail.com>
2021-02-15 13:06:45 +0530

    Added database

--- library/main.py
+++ library/main.py
@@ -1,28 +1,49 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-all_books = []
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        data = request.form
         book_name = request.form.get("book_name")
         book_author = request.form.get("book_author")
         book_rating = request.form.get("book_rating")
-        new_book = {
-            "title": book_name,
-            "author": book_author,
-            "rating": book_rating
-        }
-        all_books.append(new_book)
+
+        # new_book = {
+        #     "title": book_name,
+        #     "author": book_author,
+        #     "rating": book_rating
+        # }
+
+        new_book = Book(title=book_name, author=book_author, rating=book_rating)
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(location=url_for("home"))
     return render_template("add.html")
 
