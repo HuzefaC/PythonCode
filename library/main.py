97a8ba58c3812d15d2b83ebc0c7b5c8f006a5fa4
HuzefaC <huzefachabukswar@gmail.com>
2021-02-15 11:17:19 +0530

    Added sql_intro

--- library/main.py
+++ library/main.py
@@ -7,12 +7,24 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    if request.method == 'POST':
+        data = request.form
+        book_name = request.form.get("book_name")
+        book_author = request.form.get("book_author")
+        book_rating = request.form.get("book_rating")
+        new_book = {
+            "title": book_name,
+            "author": book_author,
+            "rating": book_rating
+        }
+        all_books.append(new_book)
+        return redirect(location=url_for("home"))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
