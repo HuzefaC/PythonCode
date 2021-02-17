e3d2df283f9e8ecb33d1ea7aaa220339d9bfffa2
HuzefaC <huzefachabukswar@gmail.com>
2021-02-17 17:52:46 +0530

    Completed library

--- library/main.py
+++ library/main.py
@@ -35,17 +35,34 @@ def add():
         book_author = request.form.get("book_author")
         book_rating = request.form.get("book_rating")
 
-        # new_book = {
-        #     "title": book_name,
-        #     "author": book_author,
-        #     "rating": book_rating
-        # }
-
         new_book = Book(title=book_name, author=book_author, rating=book_rating)
         db.session.add(new_book)
         db.session.commit()
         return redirect(location=url_for("home"))
     return render_template("add.html")
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        book_id = request.form["id"]
+        new_rating = request.form["new_rating"]
+        book_selected = Book.query.get(book_id)
+        book_selected.rating = new_rating
+        db.session.commit()
+        return redirect(location=url_for("home"))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit.html", book=book_selected)
+
+
+@app.route('/delete', methods=['GET'])
+def delete():
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    db.session.delete(book_selected)
+    db.session.commit()
+    return redirect(location=url_for("home"))
 
 
 if __name__ == "__main__":
